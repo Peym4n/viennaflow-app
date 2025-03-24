8df396b632dcd2660de8711d1bb57312f8c5c6bd
zeybera <zeyneb_tokel@hotmail.com>
2025-03-24 20:10:05 +0100

    remove logs

--- api/getRealTimeData.js
+++ api/getRealTimeData.js
@@ -4,8 +4,6 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   const { rbl } = req.query;
-  console.log("Bacnkend");
-  console.log("test");
 
   if (!rbl) {
     return res.status(400).json({ error: "rbl number ist mandatory" });
